9ffc4c8a91436083d26d42977020a79070da6b0a
Nandeesh-Gopal <nandeeshgopal26@gmail.com>
2026-05-27 15:32:26 +0530

    learn

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ console.log("currently importing")
 
 const mysql = require("mysql2");
 require("dotenv").config();
+// important to import the env variables from .env file
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -9,7 +10,26 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
+// connection pool is a collection of reusable database connection
+//Instead of creating a new database connection every time a request comes, Node.js reuses existing connections
 
+/*
+const pool = mysql.createPool({
+   host: "localhost",
+   user: "root",
+   password: "1234",
+   database: "test",
+   waitForConnections: true,
+   connectionLimit: 10,
+   queueLimit: 0
+});
+| Option               | Meaning              |
+| -------------------- | -------------------- |
+| `connectionLimit`    | Maximum connections  |
+| `waitForConnections` | Wait if all busy     |
+| `queueLimit`         | Max waiting requests |
+
+*/
 pool.getConnection((err, connection) => {
   if (err) {
     console.error(err);
